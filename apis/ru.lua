cc3a9df487a0d1584e43faeaae6ac1db2c7f38e2
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2021-09-25 22:43:04 -0400

    Create ru.lua

---
+++ apis/ru.lua
@@ -0,0 +1,21 @@
+local w,h = term.getSize()
+
+function centerWrite(text, yOffset)
+    term.setCursorPos(math.floor(w / 2 - string.len(text) / 2), h / 2 + yOffset)
+    write(text)
+end
+
+function drawOutlinedBox(xDiff, yDiff, fillColor, outlineColor)  
+    paintutils.drawFilledBox(math.floor(w / 2 - xDiff), math.floor(y / 2 - yDiff), math.floor(w / 2 + xDiff), math.floor(w / 2 + yDiff), fillColor)
+    paintutils.drawBox(math.floor(w / 2 - xDiff), math.floor(y / 2 - yDiff), math.floor(w / 2 + xDiff), math.floor(w / 2 + yDiff), outlineColor)
+end
+
+function getMid()
+    return math.floor(w / 2)
+    return math.floor(h / 2)
+end
+
+function termColors(txtColor, bgColor)
+    term.setTextColor(txtColor)
+    term.setBackgroundColor(bgColor)
+end
