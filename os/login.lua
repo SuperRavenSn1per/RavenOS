80a9e190846d2e0f33e9e46501afcddb3a93a9e2
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2021-09-26 00:16:40 -0400

    Update login.lua

--- os/login.lua
+++ os/login.lua
@@ -1,4 +1,4 @@
 term.clear()
 ru.termColors(colors.white, colors.black)
-ru.drawOutlineBox(30, 15, colors.lime, colors.green)
-ru.centerWrite("RavenOS Login", -14)
+ru.drawOutlineBox(32, 15, colors.lime, colors.green)
+ru.centerWrite(" RavenOS Login ", -14)
