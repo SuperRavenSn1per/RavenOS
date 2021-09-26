8babffc32fc4ea7022cdcb25fcbde713b957536c
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2021-09-25 22:46:02 -0400

    Update ru.lua

--- apis/ru.lua
+++ apis/ru.lua
@@ -6,8 +6,8 @@ function centerWrite(text, yOffset)
 end
 
 function drawOutlinedBox(xDiff, yDiff, fillColor, outlineColor)  
-    paintutils.drawFilledBox(math.floor(w / 2 - xDiff), math.floor(y / 2 - yDiff), math.floor(w / 2 + xDiff), math.floor(w / 2 + yDiff), fillColor)
-    paintutils.drawBox(math.floor(w / 2 - xDiff), math.floor(y / 2 - yDiff), math.floor(w / 2 + xDiff), math.floor(w / 2 + yDiff), outlineColor)
+    paintutils.drawFilledBox(math.floor(w / 2 - xDiff), math.floor(h / 2 - yDiff), math.floor(w / 2 + xDiff), math.floor(w / 2 + yDiff), fillColor)
+    paintutils.drawBox(math.floor(w / 2 - xDiff), math.floor(h / 2 - yDiff), math.floor(w / 2 + xDiff), math.floor(w / 2 + yDiff), outlineColor)
 end
 
 function getMid()
