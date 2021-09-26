923edaa6425ab4a9bcd990558b026c4904ba83af
SuperRavenSn1per <70824779+SuperRavenSn1per@users.noreply.github.com>
2021-09-25 23:02:56 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -18,18 +18,18 @@ while true do
     term.clear()
     ru.centerWrite("-[ Raven OS Startup ]-", -3)
     ru.centerWrite((sOption == 0 and "> Normal <" or "Normal"), -1)
-    ru.centerWrite((sOption == 1 and "> Safe Mode <" or "Safe Mode"), 0)
+    ru.centerWrite((sOption == 1 and "> Safe <" or "Safe"), 0)
     ru.centerWrite((sOption == 2 and "> Terminal <" or "Terminal"), 1)
     ru.centerWrite((sOption == 3 and "> Shutdown <" or "Shutdown"), 2)
     event, key = os.pullEvent("key")
     if key == 208 then
-        if sOption >= 2 then
+        if sOption <= 2 then
             sOption = sOption + 1    
         else
             sOption = 0
         end
     elseif key == 200 then
-        if sOption <= 2 then
+        if sOption >= 1 then
           sOption = sOption - 1
         else
           sOption = 3
